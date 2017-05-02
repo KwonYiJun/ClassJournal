1b3e6f38412f395f78f8ec97652f09ad6e029bb0
KwonYiJun <15031795@myrp.edu.sg>
2017-05-02 14:17:23 +0800

    Updated

--- app/src/main/java/classjournal/android/myapplicationdev/com/classjournal/addDg.java
+++ app/src/main/java/classjournal/android/myapplicationdev/com/classjournal/addDg.java
@@ -2,12 +2,31 @@ package classjournal.android.myapplicationdev.com.classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class addDg extends AppCompatActivity {
+
+    RadioGroup rgOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dg);
+
+        Button btn = (Button) findViewById(R. id. buttonSubmit);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // Get the RadioGroup object
+                RadioGroup rg = (RadioGroup) findViewById(R.id.rg);
+                // Get the Id of the selected radio button in the RadioGroup
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                // Get the radio button object from the Id we had gotten above
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+            }});
     }
 }
