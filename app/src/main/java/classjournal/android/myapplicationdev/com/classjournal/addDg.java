54271433662973b458adf98dcf7f5194ca82d9d9
KwonYiJun <15031795@myrp.edu.sg>
2017-05-02 12:41:23 +0800

    Updated

---
+++ app/src/main/java/classjournal/android/myapplicationdev/com/classjournal/addDg.java
@@ -0,0 +1,13 @@
+package classjournal.android.myapplicationdev.com.classjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addDg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dg);
+    }
+}
---
+++ app/src/main/java/classjournal/android/myapplicationdev/com/classjournal/dailyGrade.java
@@ -0,0 +1,33 @@
+package classjournal.android.myapplicationdev.com.classjournal;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15031795 on 2/5/2017.
+ */
+
+public class dailyGrade  implements Serializable{
+    private String week;
+    private String dg;
+
+    public dailyGrade(String week, String dg) {
+        this.week = week;
+        this.dg = dg;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getDg() {
+        return dg;
+    }
+
+    public void setDg(String dg) {
+        this.dg = dg;
+    }
+}
